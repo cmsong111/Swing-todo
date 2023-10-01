7beb3680bc63272990c3c51cc2ffbff7f6123fe7
Namju Kim <cmsong111@naver.com>
2023-10-02 03:53:33 +0900

    add: 차트 우클릭 기능 추가
    완료 여부 변경, 수정, 삭제기능 추가

---
+++ src/main/kotlin/com/gulio/todo_swing/view/widget/CustomPopupMenu.kt
@@ -0,0 +1,88 @@
+package com.gulio.todo_swing.view.widget
+
+
+import com.gulio.todo_swing.configuration.ApplicationContextProvider
+import com.gulio.todo_swing.repository.TodoRepository
+import com.gulio.todo_swing.view.RefreshController
+import com.gulio.todo_swing.view.TodoForm
+import java.awt.Component
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+import javax.swing.JMenuItem
+import javax.swing.JPopupMenu
+
+class CustomPopupMenu(
+    private val controller: RefreshController
+) : JPopupMenu(), MouseListener {
+
+    private val repository: TodoRepository = ApplicationContextProvider.getBean("todoRepository") as TodoRepository
+
+
+    private val logger = org.slf4j.LoggerFactory.getLogger(this.javaClass)
+
+    private val done = JMenuItem("done")
+    private val edit = JMenuItem("edit")
+    private val delete = JMenuItem("delete")
+
+    private var idx: Long = 0
+
+    init {
+        add(done)
+        add(edit)
+        add(delete)
+
+        done.addMouseListener(this)
+        edit.addMouseListener(this)
+        delete.addMouseListener(this)
+    }
+
+    fun show(invoker: Component?, x: Int, y: Int, idx: Long) {
+        super.show(invoker, x, y)
+        this.idx = idx
+    }
+
+    private fun checkPopupMenu(e: MouseEvent?) {
+        if (e == null) return
+        when (e.source) {
+            done -> {
+                logger.info("done")
+                val todo = repository.findById(idx).get()
+                todo.status = true
+                repository.save(todo)
+                controller.refresh()
+            }
+
+            edit -> {
+                logger.info("edit")
+                TodoForm(controller, idx)
+            }
+
+            delete -> {
+                logger.info("delete")
+                repository.deleteById(idx)
+                controller.refresh()
+            }
+        }
+
+    }
+
+    override fun mouseClicked(e: MouseEvent?) {
+        checkPopupMenu(e)
+    }
+
+    override fun mousePressed(e: MouseEvent?) {
+        return
+    }
+
+    override fun mouseReleased(e: MouseEvent?) {
+        checkPopupMenu(e)
+    }
+
+    override fun mouseEntered(e: MouseEvent?) {
+        return
+    }
+
+    override fun mouseExited(e: MouseEvent?) {
+        return
+    }
+}
--- src/main/kotlin/com/gulio/todo_swing/view/widget/TodoChart.kt
+++ src/main/kotlin/com/gulio/todo_swing/view/widget/TodoChart.kt
@@ -4,8 +4,13 @@ import com.gulio.todo_swing.configuration.ApplicationContextProvider
 import com.gulio.todo_swing.repository.TodoRepository
 import com.gulio.todo_swing.view.RefreshController
 import com.gulio.todo_swing.view.TodoForm
-import java.awt.Dimension
+import org.slf4j.LoggerFactory
+import java.awt.GridLayout
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import java.sql.Timestamp
 import javax.swing.JPanel
+import javax.swing.JScrollPane
 import javax.swing.JTable
 import javax.swing.table.DefaultTableModel
 
@@ -13,46 +18,77 @@ class TodoChart() : JPanel(), RefreshController {
     // Spring Bean
     private val repository: TodoRepository = ApplicationContextProvider.getBean("todoRepository") as TodoRepository
 
-    private val logger = org.slf4j.LoggerFactory.getLogger(this.javaClass)
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
+    private val scrollPane = JScrollPane()
+
+    private val popupMenu = CustomPopupMenu(this@TodoChart)
 
     // table model
-    private val tableModel = object : DefaultTableModel(
+    private val tableModel: DefaultTableModel = object : DefaultTableModel(
         arrayOf("Idx", "title", "content", "is Done", "createdDate", "updatedDate"), 0
     ) {
         override fun isCellEditable(row: Int, column: Int): Boolean {
             return false
         }
+
+        // get column class
+        // 3번째 컬럼은 checkbox 이므로 Boolean 타입으로 지정
+        override fun getColumnClass(columnIndex: Int): Class<*> {
+            return when (columnIndex) {
+                0 -> java.lang.Long::class.java
+                1 -> String::class.java
+                2 -> String::class.java
+                3 -> Boolean::class.java
+                4 -> Timestamp::class.java
+                5 -> Timestamp::class.java
+                else -> throw IllegalArgumentException("Invalid column index")
+            }
+        }
+
     }
 
     // table
     private val jTable: JTable = JTable(tableModel)
 
     init {
+        layout = GridLayout(1, 1)
         jTable.rowSelectionAllowed = true
         jTable.rowHeight = 30
-        jTable.columnModel.getColumn(0).preferredWidth = 15
+        jTable.getColumn("Idx").width = 5
 
         for (todo in repository.findAll()) {
             logger.info(todo.toString())
             tableModel.addRow(
                 arrayOf(
-                    todo.id, todo.title, todo.description, todo.status, todo.createdAt, todo.updatedAt
+                    todo.id,
+                    todo.title,
+                    todo.description,
+                    todo.status,
+                    todo.createdAt,
+                    todo.updatedAt
                 )
             )
         }
         jTable.model = tableModel
-        jTable.addMouseListener(object : java.awt.event.MouseAdapter() {
-            override fun mouseClicked(e: java.awt.event.MouseEvent) {
+        jTable.addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent) {
+
+                val row = jTable.rowAtPoint(e.point)
+                val idx = jTable.getValueAt(row, 0) as Long
+
                 if (e.clickCount == 2) {
-                    val idx = jTable.getValueAt(jTable.selectedRow, 0) as Long
                     logger.info("idx: $idx")
                     TodoForm(this@TodoChart, idx)
+                } else if (e.button == 3) {
+                    logger.info("right click :$idx")
+                    popupMenu.show(this@TodoChart, e.x, e.y, idx)
                 }
             }
-        })
 
-        jTable.preferredSize = Dimension(980, 800)
-        add(jTable)
+        })
+        scrollPane.setViewportView(jTable)
+        add(scrollPane)
     }
 
     override fun refresh() {
@@ -62,7 +98,12 @@ class TodoChart() : JPanel(), RefreshController {
             logger.info(todo.toString())
             tableModel.addRow(
                 arrayOf(
-                    todo.id, todo.title, todo.description, todo.status, todo.createdAt, todo.updatedAt
+                    todo.id,
+                    todo.title,
+                    todo.description,
+                    todo.status,
+                    todo.createdAt,
+                    todo.updatedAt
                 )
             )
         }
