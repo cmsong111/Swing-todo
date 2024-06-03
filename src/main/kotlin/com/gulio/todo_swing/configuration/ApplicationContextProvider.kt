bd8cbf13ed2d9ccc433bebea0da0894cb242c017
Namju Kim <cmsong111@naver.com>
2024-06-04 00:11:27 +0900

    docs: Add Dokka docs lib

--- src/main/kotlin/com/gulio/todo_swing/configuration/ApplicationContextProvider.kt
+++ src/main/kotlin/com/gulio/todo_swing/configuration/ApplicationContextProvider.kt
@@ -4,15 +4,34 @@ import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
 import org.springframework.stereotype.Component
 
+/**
+ * ApplicationContextProvider
+ *
+ * 해당 클래스는 스프링 컨테이너에 등록된 빈을 가져오기 위한 클래스입니다.
+ */
 @Component
 object ApplicationContextProvider : ApplicationContextAware {
 
+    /**
+     * 스프링 컨테이너 객체
+     */
     private var applicationContext: ApplicationContext? = null
 
+    /**
+     * 스프링 컨테이너에 등록된 빈을 등록하는 메소드
+     *
+     * @param context 스프링 컨테이너 객체
+     */
     override fun setApplicationContext(context: ApplicationContext) {
         applicationContext = context
     }
 
+    /**
+     * 빈을 가져오는 메소드
+     *
+     * @param beanName 빈 이름
+     * @return 빈 객체
+     */
     fun getBean(beanName: String): Any {
         return applicationContext!!.getBean(beanName)
     }
